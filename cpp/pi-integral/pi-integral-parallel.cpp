26b729011e9b8ca2f54a416c62f9421ba943c523
OctoMiao <emptymalei@qq.com>
2017-09-21 16:08:57 -0600

    update parallel code with private

--- cpp/pi-integral/pi-integral-parallel.cpp
+++ cpp/pi-integral/pi-integral-parallel.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <omp.h>
+#include <typeinfo>
 
 using namespace std;
 
@@ -7,34 +8,33 @@ using namespace std;
 
 int main(int argc, char *argv[]) {//2 parameters {# of steps, # of threads}
 
-      static long long num_steps = stoll(argv[1]);
+      const long num_steps =  1e9; //stoll(argv[1]);
  //     cout << num_steps << endl;
-      double step;
+
 
       double x, pi, sum = 0.0;
-      step = 1.0/(num_steps);
+      constexpr double step = 1.0/(num_steps);
 
-      omp_set_dynamic(0);
-      omp_set_num_threads( 5 ); // stoi( argv[2] ));
-      
+      // omp_set_dynamic(0);
+      omp_set_num_threads( stoi( argv[2] ));
 
       double tb = omp_get_wtime();
       
-      #pragma omp parallel
-      {
-            #pragma omp for reduction(+:sum)
+            #pragma omp parallel for private(x) reduction(+:sum)
             for(int i=0; i<num_steps; i++){
                   x = (i+0.5) * step;
                   sum += 4.0/(1.0+x*x);
+                  // sum += 4.0/(1.0+  (i+0.5) * step  * (i+0.5) * step  );
+                  // sum += 4.0/(1.0+  i * step  * i * step  );
             }
-//            cout << omp_get_num_threads() << endl;
-      }
+//          cout << omp_get_num_threads() << endl;
 
 
       double te = omp_get_wtime();
 
       pi = step * sum;
 
+      cout.precision(10);
       cout << argv[1] << ", " << argv[2]<< ", " << te-tb << ", " << pi << endl;
 
 }
